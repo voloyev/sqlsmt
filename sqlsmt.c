82605cc7c0bc64ac07fb9b75f235a5de0d4432a9
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-04-06 18:16:33 +0300

    Add Row and user

--- sqlsmt.c
+++ sqlsmt.c
@@ -13,6 +13,12 @@ struct InputBuffer_t
 // Create type of InputBuffer_t and named it InputBuffer
 typedef struct InputBuffer_t InputBuffer;
 
+enum ExecuteResult_t {
+        EXECUTE_SUCCESS,
+        EXECUTE_TABLE_FULL
+};
+typedef enum ExecuteResult_t ExecuteResult;
+
 enum MetaCommandResult_t {
         META_COMMAND_SUCCESS,
         META_COMMAND_UNRECOGNIZED_COMMAND
@@ -21,6 +27,7 @@ typedef enum MetaCommandResult_t MetacomandResult;
 
 enum Prepareresult_t {
         PREPARE_SUCCESS,
+        PREPARE_SYNTAX_ERROR,
         PREPARE_UNRECOGNIZED_STATMENT
 };
 typedef enum Prepareresult_t PrepareResult;
@@ -31,11 +38,52 @@ enum StatmentType_t {
 };
 typedef enum StatmentType_t StatementType;
 
+const uint32_t COLUMN_USERNAME_SIZE = 32;
+const uint32_t COLUMN_EMAIL_SIZE = 255;
+
+struct Row_t {
+        uint32_t id;
+        char username[COLUMN_USERNAME_SIZE];
+        char email[COLUMN_EMAIL_SIZE];
+};
+typedef struct Row_t Row;
+
 struct Statment_t {
         StatementType type;
+        Row row_to_insert;
 };
 typedef struct Statment_t Statment;
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t TABLE_MAX_PAGES = 100;
+const uint32_t ROW_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROW_PER_PAGE * TABLE_MAX_PAGES;
+
+struct Table_t {
+        void* pages[TABLE_MAX_PAGES];
+        uint32_t num_rows;
+};
+typedef struct Table_t Table;
+
+void print_row(Row* row) {
+        printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+void serialize_row(Row* source, void* destination) {
+        memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+        memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+        memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+// IMPLEMENT!
 InputBuffer* new_input_buffer()
 {
         //alllocate memmory for input
@@ -55,7 +103,8 @@ void print_prompt()
 
 void read_input(InputBuffer* input_buffer)
 {
-        ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+        ssize_t bytes_read = getline(&(input_buffer->buffer),
+                                     &(input_buffer->buffer_length), stdin);
 
         if (bytes_read <= 0) {
                 printf("Error reading input\n");
@@ -77,7 +126,13 @@ MetacomandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statment(InputBuffer* input_buffer, Statment* statment) {
         if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
                 statment->type = STATMENT_INSERT;
-                return PREPARE_SUCCESS;
+                int args_assigned = sscanf(
+                        input_buffer->buffer, "Insert %d %s %s",
+                        &(statment->row_to_insert.id),
+                        statment->row_to_insert.username, statment->row_to_insert.email);
+                if (args_assigned < 3) {
+                        return PREPARE_SUCCESS;
+                }
         }
         if (strcmp(input_buffer->buffer, "select") == 0) {
                 statment->type = STATMENT_SELECT;
@@ -96,7 +151,7 @@ void execute_statment(Statment* statement) {
                 printf("This is where we would do a select.\n");
                 break;
         }
-        
+
 }
 
 int main(int argc, char* argv[])
