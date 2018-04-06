238001cfffb777a0142223593250986ca1470b11
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-04-06 15:18:05 +0300

    Add basic enums

--- sqlsmt.c
+++ sqlsmt.c
@@ -2,13 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// Here init structture
-// buffer with char pointer
-// As an implication, size_t is a type guaranteed to hold any array index : buffer_length
-//
-// int can store values of at least the range [-2^15 ... 2^15-1] per ISO C
-// ssize_t can store values of at least the range [-1 ... 2^15-1] per POSIX (see _POSIX_SSIZE_MAX).
-// its about input_length
+
 struct InputBuffer_t
 {
         char* buffer;
@@ -18,13 +12,34 @@ struct InputBuffer_t
 
 // Create type of InputBuffer_t and named it InputBuffer
 typedef struct InputBuffer_t InputBuffer;
-// Create function with InputBuffer type
+
+enum MetaCommandResult_t {
+        META_COMMAND_SUCCESS,
+        META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetacomandResult;
+
+enum Prepareresult_t {
+        PREPARE_SUCCESS,
+        PREPARE_UNRECOGNIZED_STATMENT
+};
+typedef enum Prepareresult_t PrepareResult;
+
+enum StatmentType_t {
+        STATMENT_INSERT,
+        STATMENT_SELECT
+};
+typedef enum StatmentType_t StatementType;
+
+struct Statment_t {
+        StatementType type;
+};
+typedef struct Statment_t Statment;
+
 InputBuffer* new_input_buffer()
 {
         //alllocate memmory for input
         InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
-        // foo->bar is equivalent to (*foo).bar, i.e. it gets the member called bar from the struct that foo points to.
-        // assign fields of InputBuffer_t to some values
         input_buffer->buffer = NULL;
         input_buffer->buffer_length = 0;
         input_buffer->input_length = 0;
@@ -40,23 +55,6 @@ void print_prompt()
 
 void read_input(InputBuffer* input_buffer)
 {
-        // &a; returns the actual address of the variable.
-        /*
-         * Here’s a typical getline() statement:
-         *
-         *          getline(&buffer,&size,stdin);
-         *
-         *          The getline() function is prototyped in the stdio.h header file. Here are the three arguments:
-         *
-         *          &buffer is the address of the first character position where the input string will be stored.
-         *          It’s not the base address of the buffer, but of the first character in the buffer.
-         *           This pointer type (a pointer-pointer or the ** thing) causes massive confusion.
-         *
-         *          &size is the address of the variable that holds the size of the input buffer, another pointer.
-         *
-         *          stdin is the input file handle. So you could use getline() to read a line of text from a file,
-         *           but when stdin is specified, standard input is read.
-         */
         ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
         if (bytes_read <= 0) {
@@ -64,11 +62,41 @@ void read_input(InputBuffer* input_buffer)
                 exit(EXIT_FAILURE);
         }
 
-        // Ignore trailing newline
-        // remove newline 
         input_buffer->input_length = bytes_read - 1;
-        // add 0 at the end of a buffer
         input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+MetacomandResult do_meta_command(InputBuffer* input_buffer) {
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+                exit(EXIT_SUCCESS);
+        } else {
+                return META_COMMAND_UNRECOGNIZED_COMMAND;
+        }
+}
+
+PrepareResult prepare_statment(InputBuffer* input_buffer, Statment* statment) {
+        if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+                statment->type = STATMENT_INSERT;
+                return PREPARE_SUCCESS;
+        }
+        if (strcmp(input_buffer->buffer, "select") == 0) {
+                statment->type = STATMENT_SELECT;
+                return PREPARE_SUCCESS;
+        }
+
+        return PREPARE_UNRECOGNIZED_STATMENT;
+}
+
+void execute_statment(Statment* statement) {
+        switch (statement->type) {
+        case(STATMENT_INSERT):
+                printf("this is where we would do an insert.\n");
+                break;
+        case (STATMENT_SELECT):
+                printf("This is where we would do a select.\n");
+                break;
+        }
+        
 }
 
 int main(int argc, char* argv[])
@@ -77,13 +105,25 @@ int main(int argc, char* argv[])
         while (true) {
                 print_prompt();
                 read_input(input_buffer);
-
-                if (strcmp(input_buffer->buffer, ".exit") == 0) {
-                        // when command .exit than exit the buffer
-                        puts("Bye!");
-                        exit(EXIT_SUCCESS);
-                } else {
-                        printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                if (input_buffer->buffer[0] == '.') {
+                        switch (do_meta_command(input_buffer)) {
+                        case (META_COMMAND_SUCCESS):
+                                continue;
+                        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                                continue;
+                        }
                 }
+                Statment statment;
+                switch (prepare_statment(input_buffer, &statment)) {
+                case (PREPARE_SUCCESS):
+                        break;
+                case (PREPARE_UNRECOGNIZED_STATMENT):
+                        printf("Unrecognized keyword at start of '%s'\n",
+                               input_buffer->buffer);
+                        continue;
+                }
+                execute_statment(&statment);
+                printf("Executed.\n");
         }
 }
