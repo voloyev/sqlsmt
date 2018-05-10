8bef163ccaea8c61c5edc7589975c6e65e26df44
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-05-10 10:28:57 +0300

    finish chapter 3

--- sqlsmt.c
+++ sqlsmt.c
@@ -150,10 +150,12 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
         if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
                 statement->type = STATEMENT_INSERT;
                 int args_assigned = sscanf(
-                        input_buffer->buffer, "Insert %d %s %s",
+                        input_buffer->buffer, "insert %d %s %s",
                         &(statement->row_to_insert.id),
-                        statement->row_to_insert.username, statement->row_to_insert.email);
+                        statement->row_to_insert.username,
+                        statement->row_to_insert.email);
                 if (args_assigned < 3) {
+                        printf("%d", args_assigned);
                         return PREPARE_SYNTAX_ERROR;
                 }
                 return PREPARE_SUCCESS;
